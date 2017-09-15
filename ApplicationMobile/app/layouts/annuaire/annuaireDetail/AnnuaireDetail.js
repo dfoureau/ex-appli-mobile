2cd25d31d099f5b4d963f7a7b52167262f0b50fb
braclet <b.raclet@cat-amania.com>
2017-09-15 17:43:06 +0200

    IHM annuaire détail

--- ApplicationMobile/app/layouts/annuaire/annuaireDetail/AnnuaireDetail.js
+++ ApplicationMobile/app/layouts/annuaire/annuaireDetail/AnnuaireDetail.js
@@ -12,7 +12,8 @@ import {
 Alert,
 ScrollView  } from 'react-native'
 import { StackNavigator, NavigationActions } from 'react-navigation';
-import StyleGlobal from '../../../styles/Styles';
+import Style from '../../../styles/Styles';
+import styles from './styles';
 
 import ContainerAccueil from '../../../components/containerAccueil/ContainerAccueil';
 import { ContainerHeader } from '../../../components/containerHeader';
@@ -25,7 +26,7 @@ class AnnuaireDetail extends React.Component {
 	constructor (props) {
 		super(props)
 		this.state = {
-            titre:"Annuaire Detail",
+            titre:"Alain AFFLELOU",
 			//On définit les différentes variables
 		}
 	}
@@ -48,8 +49,109 @@ class AnnuaireDetail extends React.Component {
             
             <View>
                 <ContainerAccueil title={this.state.titre} afficherEcran={this.afficherEcranParent.bind(this)}>
-                <ScrollView>
+                <ScrollView style={styles.scrollView}>
 
+                    {/*DESCRIPTION PROFILE*/}
+                    <View style={Style.firstView}>
+                        <View style={Style.secondView}>
+                            <View style={styles.container}>
+                                <View style={styles.containerRow}>
+                                    <Text style={styles.text}>Entité: CAT-AMANIA</Text>
+                                    <Text style={styles.text}>Fonction: Directeur d'agence</Text>
+                                    <Text style={styles.text}>Agence: Nantes</Text>
+                                </View>
+                            </View>
+                        </View>
+                        <View style={styles.containerIcon}>
+                            <Image style={styles.iconProfile} source={require('../../../images/imageProfilDefault.png')}/>
+                        </View>
+                    </View>
+                        
+                    {/*TELEPHONE 1*/}
+                    <View style={[Style.firstView, styles.firstSection]}>
+                        <View style={Style.secondView}>
+                            <View style={styles.container}>
+                                <View style={styles.containerRow}>
+                                    <Text style={styles.text}>06.06.06.06.06</Text>
+                                </View>
+                            </View>
+                        </View>
+                        <View style={styles.containerIcon}>
+                            <TouchableHighlight onPress={null}>
+                                <View>
+                                    <Image style={styles.icon} source={require('../../../images/icons/tel.png')}/>
+                                </View>
+                            </TouchableHighlight>
+                        </View>
+                        <View style={styles.containerIcon}>
+                            <TouchableHighlight onPress={null}>
+                                <View>
+                                    <Image style={styles.icon} source={require('../../../images/icons/bulles.png')}/>
+                                </View>
+                            </TouchableHighlight>
+                        </View>
+                    </View>
+
+                    {/*TELEPHONE 2*/}
+                    <View style={[Style.firstView, styles.secondSection]}>
+                        <View style={Style.secondView}>
+                            <View style={styles.container}>
+                                <View style={styles.containerRow}>
+                                    <Text style={styles.text}>06.06.06.06.06</Text>
+                                </View>
+                            </View>
+                        </View>
+                        <View style={styles.containerIcon}>
+                            <TouchableHighlight onPress={null}>
+                                <View>
+                                    <Image style={styles.icon} source={require('../../../images/icons/tel.png')}/>
+                                </View>
+                            </TouchableHighlight>
+                        </View>
+                        <View style={styles.containerIcon}>
+                            <TouchableHighlight onPress={null}>
+                                <View>
+                                    <Image style={styles.icon} source={require('../../../images/icons/bulles.png')}/>
+                                </View>
+                            </TouchableHighlight>
+                        </View>
+                    </View>
+
+                    {/*EMAIL 1 */}
+                    <View style={[Style.firstView, styles.firstSection]}>
+                        <View style={Style.secondView}>
+                            <View style={styles.container}>
+                                <View style={styles.containerRow}>
+                                    <Text style={styles.text}>a.afflelou@email.cat.com</Text>
+                                </View>
+                            </View>
+                        </View>
+                        <View style={styles.containerIcon}>
+                            <TouchableHighlight onPress={null}>
+                                <View>
+                                    <Image style={styles.icon} source={require('../../../images/icons/email.png')}/>
+                                </View>
+                            </TouchableHighlight>
+                        </View>
+                    </View>
+
+                    {/*EMAIL 2*/}
+                    <View style={[Style.firstView, styles.secondSection]}>
+                        <View style={Style.secondView}>
+                            <View style={styles.container}>
+                                <View style={styles.containerRow}>
+                                    <Text style={styles.text}>a.afflelou@email.cat.client.com</Text>
+                                </View>
+                            </View>
+                        </View>
+                        <View style={styles.containerIcon}>
+                            <TouchableHighlight onPress={null}>
+                                <View>
+                                    <Image style={styles.icon} source={require('../../../images/icons/email.png')}/>
+                                </View>
+                            </TouchableHighlight>
+                        </View>
+                    </View>
 
                 </ScrollView>
                 </ContainerAccueil>
--- ApplicationMobile/app/layouts/annuaire/annuaireDetail/styles.js
+++ ApplicationMobile/app/layouts/annuaire/annuaireDetail/styles.js
@@ -7,29 +7,46 @@ import { StyleSheet,
   const bleu2 = "#2ABBDD";
   const bleu3 = "#355A86";
   export default   StyleSheet.create({
-      scrollView:{ width:width,height:height,backgroundColor:'#fff',},
-      viewContainer:{marginTop:150, width:width, justifyContent:'center',alignItems: 'center',},
-      viewChamps:{justifyContent:'center',alignItems: 'center',marginTop:120},
-      viewSeConnecter:{marginTop:20, alignItems: 'center', },
-      btnSeconnecter:{fontSize:30,},
-      viewMdpOublie:{marginTop:20, alignItems: 'center', },
-      touchMdpOublie:{width:200,height:50, justifyContent:'center',alignItems: 'center',},
-      txtMdpOublie:{fontSize:15, color:"#2298AA", justifyContent:'center',alignItems: 'center',},
-      inputContainer: {
-          borderLeftWidth: 2,
-          borderRightWidth: 2,
-          borderTopWidth: 2,
-          borderBottomWidth: 2,
-          height: 50,
-          marginTop:20,
-        },
-  
-      input: {
-          height: 50,
-          backgroundColor: '#ffffff',
-          paddingLeft: 15,
-          paddingRight: 15,
-          borderWidth: 0, 
-          fontSize:19,
-        }
+      scrollView:{ 
+        width:width,
+        height:height,
+        backgroundColor:'#fff'
+      },
+      container: {
+        padding: 10,
+        paddingTop: 20,
+        width:width,
+      },
+      firstSection: {
+        paddingTop: 27,
+      },
+      secondSection: {
+        paddingTop: 5,
+      },
+      containerRow: {
+        flex:1,
+        paddingLeft: 10,
+        width:width,
+      },
+      text: {
+        fontSize: 16,
+      },
+      containerIcon: {
+        marginRight: 10,
+        flexDirection:'row',
+        flex: .20,
+        alignItems: 'center',
+      },
+      iconProfile: {
+        height:55,
+        width:55,
+        borderRadius:50, 
+        marginTop: 20,
+      },
+      icon: {
+        height:55,
+        width:55,
+        //borderRadius:50,
+        marginTop: 5 
+      },
   });
