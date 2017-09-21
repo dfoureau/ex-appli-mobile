c633cd1c0e29ab0aaa4605e1ff40d183aec38df3
Jessica MARMIGNON <j.marmignon@cat-amania.com>
2017-09-21 15:40:43 +0200

    page accueil

--- ApplicationMobile/app/layouts/accueil/Styles.js
+++ ApplicationMobile/app/layouts/accueil/Styles.js
@@ -1,35 +1,72 @@
-import { StyleSheet,
-    Dimensions, } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
   
-  
-  var {height, width} = Dimensions.get('window');
-  const bleu1 = "#2298AA";
-  const bleu2 = "#2ABBDD";
-  const bleu3 = "#355A86";
+var {height, width} = Dimensions.get('window');
+
   export default   StyleSheet.create({
-      scrollView:{ width:width,height:height,backgroundColor:'#fff',},
-      viewContainer:{marginTop:150, width:width, justifyContent:'center',alignItems: 'center',},
-      viewChamps:{justifyContent:'center',alignItems: 'center',marginTop:120},
-      viewSeConnecter:{marginTop:20, alignItems: 'center', },
-      btnSeconnecter:{fontSize:30,},
-      viewMdpOublie:{marginTop:20, alignItems: 'center', },
-      touchMdpOublie:{width:200,height:50, justifyContent:'center',alignItems: 'center',},
-      txtMdpOublie:{fontSize:15, color:"#2298AA", justifyContent:'center',alignItems: 'center',},
-      inputContainer: {
-          borderLeftWidth: 2,
-          borderRightWidth: 2,
-          borderTopWidth: 2,
-          borderBottomWidth: 2,
-          height: 50,
-          marginTop:20,
-        },
-  
-      input: {
-          height: 50,
-          backgroundColor: '#ffffff',
-          paddingLeft: 15,
-          paddingRight: 15,
-          borderWidth: 0, 
-          fontSize:19,
-        }
+      container1: {
+        flexDirection: 'column'
+      },
+
+      containerUser: {
+        flex: 0.25,
+        flexDirection: 'column',
+        marginHorizontal: 10,
+        paddingHorizontal:5,
+        paddingVertical: 5,
+        borderWidth:0.5,
+        borderColor: "#808080",
+        borderRadius:5
+      },
+
+      containerVacation: {
+        flex: 0.25,
+        flexDirection: 'column',
+        marginHorizontal: 10,
+        paddingHorizontal:5,
+        paddingVertical: 5,
+        borderWidth:0.5,
+        borderColor: "#808080",
+        borderRadius:5,
+        alignItems: 'stretch'
+      },
+
+      containerNews: {
+        flex: 0.5,
+        flexDirection: 'column',
+        marginHorizontal: 10,
+        paddingHorizontal:5,
+        paddingVertical: 5,
+        borderWidth:0.5,
+        borderColor: "#808080",
+        borderRadius:5,
+        alignItems: 'stretch'
+      },
+
+      text: {
+        color: '#000000',
+        fontSize: 12
+      },
+
+      text2: {
+        paddingHorizontal:25
+      },
+
+      titleContainer: {
+        paddingTop: 10,
+        paddingHorizontal:15,
+        color: '#000000',
+        fontSize: 14,
+        fontWeight: 'bold',
+      },
+
+      containerList: {
+        marginBottom: 20
+      },
+
+      titleNews: {
+        color: '#000000',
+        fontSize: 12,
+        fontWeight: 'bold',
+        paddingBottom: 5
+      }
   });
