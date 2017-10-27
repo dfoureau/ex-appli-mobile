248c2d2a7acf04f3202657b8eeff33a5e217bf41
Adel N <a.noureddine@cat-amania.com>
2017-10-27 10:17:16 +0200

    mise à jour service NDF

--- ServicesREST/Symfony/src/AppBundle/Controller/NdfController.php
+++ ServicesREST/Symfony/src/AppBundle/Controller/NdfController.php
@@ -11,6 +11,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use AppBundle\Security\LoginController;
 
 
@@ -29,12 +30,22 @@ class NdfController extends Controller
             return new JsonResponse($retourAuth,403);
           }*/
 
-         $sql ='SELECT idUser as id, CASE 
+		$id = (int) $id;
+		$annee = (int) $annee;
+		$mois = (int) $mois;
+		
+		
+		if (is_int($id) && is_int($annee) && is_int($mois)) 
+		{
+
+
+
+		$sql ='SELECT idUser as id, CASE 
          when etat="3" then "A modifier"
          when etat="2" then "Validé" 
-         when etat="1" then "En attente de validation"
+         when etat="1" then "En attente validation"
          when etat="0" then "Brouillon" 
-         end as etat,jour, mois, annee, facturable,client, lieu, coalesce(forfait,0.00) as forfait, nbkms,coalesce(sncf,0.00) as sncf, coalesce(peages,0.00) as peage,essence, coalesce(taxi,0.00) as taxi,indemKM,notedefrais.nbzones,pourcentage,coalesce(hotel,0.00) as hotel,coalesce(repas,0.00) as repas,coalesce(invit,0.00) as invit, divers, libelle FROM notedefrais WHERE idUser = "'.$id.'" AND mois = "'.$mois.'" AND annee = "'.$annee.'"';
+         end as etat,jour, mois, annee, facturable,client, lieu, coalesce(forfait,0.00) as forfait, nbkms,coalesce(sncf,0.00) as sncf, coalesce(peages,0.00) as peages,essence, coalesce(parking,0.00) as parking,coalesce(taxi,0.00) as taxi,indemKM,notedefrais.nbzones,pourcentage,coalesce(hotel,0.00) as hotel,coalesce(repas,0.00) as repas,coalesce(invit,0.00) as invit, divers, libelle FROM notedefrais WHERE idUser = "'.$id.'" AND mois = "'.$mois.'" AND annee = "'.$annee.'"';
 
         $stmt = $this->getDoctrine()->getManager()->getConnection()->prepare($sql);
         $stmt->execute();
@@ -65,8 +76,9 @@ class NdfController extends Controller
             $rowNdf['montantForfait']=$row['forfait'];
             $rowNdf['nbKM']=$row['nbkms'];
             $rowNdf['montantFraisSNCF']=$row['sncf'];
-            $rowNdf['montantPeage']=$row['peage'];
+            $rowNdf['montantPeages']=$row['peages'];
             $rowNdf['montantEssence']=$row['essence'];
+			$rowNdf['montantParking']=$row['parking'];
             $rowNdf['montantTaxi']=$row['taxi'];
             $rowNdf['indemKM']=$row['indemKM'];
             $rowNdf['montantNbZone']=$row['nbzones'];
@@ -80,15 +92,20 @@ class NdfController extends Controller
           }
           $retour=array('idUser'=>$id, 'mois'=>$mois,'annee'=>$annee,'etat'=>$etat, 'notesDeFrais'=>$listNdf);
           
-            return new JsonResponse($retour,200);
+            return new JsonResponse($retour,Response::HTTP_OK);
        }
        else
        { 
-       $message=array('message'=>'Introuvable');
-       return new JsonResponse($message,400);
+       $message=array('message'=>'Pas de données');
+       return new JsonResponse($message,Response::HTTP_NOT_FOUND);
        }
+	 } 
+	  else {
+			$message = array('message' => 'Format parametres incorrect');
+			return new JsonResponse($message, Response::HTTP_BAD_REQUEST);
 
     }
+}	
     /**
      * @Route("/ndf/{id}/{annee}/{mois}", name="ndf2")
        @Method("DELETE")
@@ -108,33 +125,66 @@ class NdfController extends Controller
     public function deleteNdf($id,$annee,$mois)
     {
 
+		//on commence par vérifier les variables en entrée de la request
+		$id = (int) $id;
+		$annee = (int) $annee;
+		$mois = (int) $mois;
+		$etat = null;
+		
+		
+		if (is_int($id) && is_int($annee) && is_int($mois)) 
+		{
+	
+	
        //on vérifie si la ligne existe
-        $sql ='select * FROM notedefrais WHERE  etat != "2" and  idUser = "'.$id.'" AND mois = "'.$mois.'" AND annee = "'.$annee.'"';
+        $sql ='select etat FROM notedefrais WHERE  idUser = "'.$id.'" AND mois = "'.$mois.'" AND annee = "'.$annee.'" group by etat' ;
         $stmt = $this->getDoctrine()->getManager()->getConnection()->prepare($sql);
         $stmt->execute();
 
         $list= $stmt->fetchAll();
 
         //si la ligne existe, alors on la supprime
-       if(count($list)!=0){
+		
+           for($i=0; $i<count($list);$i++){
+            $row=$list[$i];
+            $etat=$row['etat'];
+			}
+		
+			if($etat == '0' || $etat == '1' || $etat == '3'){
+				/*
+			 when etat="3" then "A modifier"
+			 when etat="2" then "Validé" 
+			 when etat="1" then "En attente validation"
+			 when etat="0" then "Brouillon" */
 
 
-        $sql ='DELETE FROM notedefrais WHERE  idUser = "'.$id.'" AND mois = "'.$mois.'" AND annee = "'.$annee.'"';
+			$sql ='DELETE FROM notedefrais WHERE  idUser = "'.$id.'" AND mois = "'.$mois.'" AND annee = "'.$annee.'"';
 
-        $stmt = $this->getDoctrine()->getManager()->getConnection()->prepare($sql);
-        $retour=$stmt->execute();
-       
-        $message=array('message'=>'Note de frais supprimee');
-        
-        return  array('message'=>$message,'code'=>200);
-       }
-       //si la ligne n'existe pas, alors on le dit et on ne supprime pas
-       else
-        {
-          $message=array('message'=>'Delete KO');
-          return  array('message'=>$message,'code'=>400);
-        }
+			$stmt = $this->getDoctrine()->getManager()->getConnection()->prepare($sql);
+			$retour=$stmt->execute();
+		   
+			$message=array('message'=>'Delete OK: Note de frais supprimee');
+			
+			return  array('message'=>$message,'code'=>Response::HTTP_OK);
+			}
+				//si la ligne existe mais validee
+			else if ($etat == 2)
+				{
+				  $message=array('message'=>'Delete KO: La note de frais est validee');
+				  return  array('message'=>$message,'code'=>Response::HTTP_NOT_FOUND);
+				}
+				 //si la ligne n'existe pas, alors on le dit et on ne supprime pas
+			else
+				{
+				  $message=array('message'=>'Delete KO: Pas de ligne à supprimer');
+				  return  array('message'=>$message,'code'=>Response::HTTP_NOT_FOUND);
+				}
+			}
+		else {
+				$message = array('message' => 'Delete KO: Format parametres incorrect');
+				return array('message'=>$message,'code'=>Response::HTTP_BAD_REQUEST);
 
+		}
     }
     
     /**
@@ -143,12 +193,54 @@ class NdfController extends Controller
      */
     public function putNdfAction(Request $request,$id,$annee,$mois)
     {
+		
+		
 
          /*$log=new LoginController();
         $retourAuth = $log->checkAuthentification($this);
         if (array_key_exists("erreur", $retourAuth)) {
             return new JsonResponse($retourAuth,400);
           }*/
+		  
+		  
+		 /* Exemple de Jason
+			{
+				"idUser": "124124251",
+				"mois": "9",
+				"annee": "2017",
+				"etat": "Brouillon",
+				"notesDeFrais": [
+					{
+						"jour": "3",
+						"mois": "9",
+						"annee": "2017",
+						"facturable": "0",
+						"client": "CIMUT",
+						"lieu": "Paris",
+						"montantForfait": "00.00",
+						"nbKM": "585",
+						"montantFraisSNCF": "0.00",
+						"montantPeages": "0.00",
+						"montantParking": "0.00",
+						"montantEssence": "0.00",
+						"montantTaxi": "0.00",
+						"montantNbZone": "0",
+						"montantPourcentage": "0.00",
+						"montantHotel": "0.00",
+						"montantRepas": "18.40",
+						"montantInvitation": "0.00",
+						"montantDivers": "0.00",
+						"libelleDivers": ""
+					}
+					
+				]
+			}
+			*/		 
+		//on verifie que les données en entrée 
+		
+		// Get the entity manager
+
+		  
         //on appelle la fonction deleteNDF
         $retourdelete=$this->deleteNdf($id,$annee,$mois);  
         if($retourdelete['code']!=200){
@@ -156,8 +248,8 @@ class NdfController extends Controller
         }
    
         //on appelle la fonction postNDF
-        $retourpost = $this->postNdfAction($request);  
-          return $retourpost;
+        $retourpost = $this->postNdfAction($request); 		
+		  return $retourpost;
 
          if($retourpost['code']!=200){
           return new JsonResponse($retourpost['message'],$retourpost['code']);
@@ -179,7 +271,7 @@ class NdfController extends Controller
 	
        if((int)($idUser)<=0){
            $message=array('message'=>'Paramètre idUser incorrect');
-            return new JsonResponse($message,400);
+            return new JsonResponse($message,Response::HTTP_BAD_REQUEST);
        }
         
       $sql='SELECT idUser,  mois, annee, COALESCE(ROUND(SUM(nbkms)*MIN(notedefrais.indemKM),2),0) + COALESCE(SUM(peages),0) + COALESCE(SUM(notedefrais.forfait),0) + COALESCE(SUM(sncf),0) + COALESCE(SUM(pourcentage),0) + COALESCE(SUM(hotel),0) + COALESCE(SUM(repas),0) + COALESCE(SUM(invit),0) + COALESCE(SUM(essence),0) + COALESCE(SUM(parking),0) + COALESCE(SUM(taxi),0) + COALESCE(SUM(divers),0) AS montantTotal, CASE etat WHEN 0 THEN "Brouillon" WHEN 1 THEN "En attente validation" WHEN 2 THEN "Validé" WHEN 2 THEN "A modifier" ELSE "Autre" END as etat, dateactionetat, CONCAT(valid.prenom," ", valid.nom) as valideur FROM notedefrais  left join users as valid on notedefrais.validateur = valid.id WHERE idUser = "'.$idUser.'" and annee = '.$annee.' GROUP BY idUser, mois, annee,etat, dateactionetat;';
@@ -189,12 +281,13 @@ class NdfController extends Controller
        	$retour= $stmt->fetchAll();
        	if(count($retour)==0){
          	$message=array('message'=>'Aucune note de frais trouvée');
-            return new JsonResponse($message,400);
+            return new JsonResponse($message,Response::HTTP_NOT_FOUND);
        	}
        else 
        	return new JsonResponse($retour);
 
     }
+
 
   /**
   * @Route("/ndf/", name="ndfPost")
@@ -207,12 +300,48 @@ class NdfController extends Controller
         if (array_key_exists("erreur", $retourAuth)) {
             return new JsonResponse($retourAuth,400);
           }*/ 
+		  
+		  
+			 /* Exemple de Jason
+			{
+				"idUser": "124124251",
+				"mois": "9",
+				"annee": "2017",
+				"etat": "Brouillon",
+				"notesDeFrais": [
+					{
+						"jour": "3",
+						"mois": "9",
+						"annee": "2017",
+						"facturable": "0",
+						"client": "CIMUT",
+						"lieu": "Paris",
+						"montantForfait": "00.00",
+						"nbKM": "585",
+						"montantFraisSNCF": "0.00",
+						"montantPeages": "0.00",
+						"montantParking": "0.00",
+						"montantEssence": "0.00",
+						"montantTaxi": "0.00",
+						"montantNbZone": "0",
+						"montantPourcentage": "0.00",
+						"montantHotel": "0.00",
+						"montantRepas": "18.40",
+						"montantInvitation": "0.00",
+						"montantDivers": "0.00",
+						"libelleDivers": ""
+					}
+					
+				]
+			}
+			*/  
+		  
       $data = json_decode(file_get_contents('php://input'), true);
      try{
         $retour = $this->postNdf($data);
      }
      catch (\Symfony\Component\Debug\Exception\ContextErrorException $e) {
-        return new JsonResponse("Problème de paramètres ", 400);
+        return new JsonResponse("Problème de paramètres ", Response::HTTP_BAD_REQUEST);
       }
       return new JsonResponse($retour['message'],$retour['code']);
     }
@@ -225,6 +354,7 @@ class NdfController extends Controller
         if (array_key_exists("erreur", $retourAuth)) {
             return new JsonResponse($retourAuth,400);
           }*/
+		
         $idUser = $data['idUser'];
         $mois = $data['mois'];
         $annee = $data['annee'];
@@ -239,14 +369,14 @@ class NdfController extends Controller
               break;
             //Etats validés ou A modifier interdits, autres états inconnus
             default :
-              $retour=array('code'=>'400', 'message'=>'Etat invalide');
+              $retour=array('message'=>'Etat invalide','code'=>Response::HTTP_BAD_REQUEST);
               return $retour;
               break;       
         }
         
         //récupérer indemKM depuis table users
-        $indemKM = number_format($this->getUserIndemKM($idUser), 3, ',','');
-
+        $indemKM = number_format($this->getUserIndemKM($idUser), 3, '.','');
+		//echo $indemKM;
         $sql='INSERT into notedefrais (idUser, jour, mois, annee, etat, client, lieu, facturable, nbkms, peages, forfait, sncf, nbzones, pourcentage,fdr, hotel, repas, invit, essence, parking, taxi, divers, libelle, indemKM, validateur, dateactionetat) VALUES ';
 
 
@@ -281,7 +411,7 @@ class NdfController extends Controller
             }
 
 
-            $sql .= "('$idUser', '$jour', '$mois', '$annee', $etat, '$client', '$lieu', '$facturable', '$nbKM', '$montantPeages', '$montantForfait', '$montantFraisSNCF', '$montantNbZone', '$montantPourcentage','$montantFdr', '$montantHotel', '$montantRepas', '$montantInvitation', '$montantEssence', '$montantParking', '$montantTaxi', '$montantDivers', '$libelleDivers', '$indemKM', '$idUser', now())"; 
+            $sql .= "('$idUser', '$jour', '$mois', '$annee', $etat, '$client', '$lieu', '$facturable', '$nbKM', '$montantPeages', '$montantForfait', '$montantFraisSNCF', '$montantNbZone', '$montantPourcentage','$montantFdr', '$montantHotel', '$montantRepas', '$montantInvitation', '$montantEssence', '$montantParking', '$montantTaxi', '$montantDivers', '$libelleDivers', $indemKM, '$idUser', now())"; 
 
             $i++;
           }
@@ -291,7 +421,7 @@ class NdfController extends Controller
       $stmt = $this->getDoctrine()->getManager()->getConnection()->prepare($sql);
       $stmt->execute();
 
-      $retour=array('message'=>"OK",'code'=>'200' );
+      $retour=array('message'=>"La note de frais a ete creee/mise a jour",'code'=>Response::HTTP_OK );
       return $retour;
 
     }
@@ -310,8 +440,9 @@ class NdfController extends Controller
       $stmt->execute();
 
       $retour= $stmt->fetch();
-
+		
       return $retour['indemKM'];
+	  
     }
 	
 	
