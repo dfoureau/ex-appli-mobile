d3a61b63f57d69df4025bd3595e99cdeb3dbba30
alami jonathan <j.alami@cat-amania.com>
2017-10-31 10:30:27 +0000

    Controle valeurs en entree

--- ServicesREST/Symfony/src/AppBundle/Controller/NewsController.php
+++ ServicesREST/Symfony/src/AppBundle/Controller/NewsController.php
@@ -5,55 +5,70 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use AppBundle\Security\LoginController;
 
 class NewsController extends Controller
 {
+	/**
+	 * @Route("/news/{nombre}", name="news")
+	 * @Method({"GET"})
+	 */
+	public function news(Request $request, $nombre)
+	{
+		// $log = new LoginController();
+		// $retourAuth = $log->checkAuthentification($this);
+		// if (array_key_exists("erreur", $retourAuth)) {
+		// 	return new JsonResponse($retourAuth, Response::HTTP_FORBIDDEN);
+		// }
 
+		// Test valeur en entrée
+		if (UtilsController::isPositifInt($nombre)) {
 
-    /**
-	* @Route("/news/{nombre}", name="news")
-	* @Method({"GET"})
-     */
-    public function news(Request $request, $nombre)
-    {
-		/*$log=new LoginController();
-        $retourAuth = $log->checkAuthentification($this);
-        if (array_key_exists("erreur", $retourAuth)) {
-            return new JsonResponse($retourAuth,403);
-          }*/
-  
-      if((int)($nombre)<=0){
-           $message=array('message'=>'Paramètre nombre incorrect');
-            return new JsonResponse($message,400);
-       }
+			$tNombre = (int) $nombre;
 
+// Récupérer l'id de l'utilisateur dans le token
+// $idUser = $retourAuth['id'];
+// Pour les tests on prend un id fixe
+$idUser = 124123958;
 
-    	//récupérer l'id de l'utilisateur dans le token
-		//$idUser = $retourAuth['id'];
-		// Pour les tests on prend un id fixe
-    	$idUser = 124123958;
+//TODO mettre les paramètres dans un fichier de config
+			$chemin_photo = '/espacecollaborateur/upload/news/photo/';
+			$chemin_pdf = '/espacecollaborateur/upload/news/doc/';
 
-    	//TODO mettre les paramètres dans un fichier de config
-    	$chemin_photo = '/espacecollaborateur/upload/news/photo/';
-    	$chemin_pdf = '/espacecollaborateur/upload/news/doc/';
+			$sql = 'SELECT 
+						news_id,news_titre,
+						news_contenu, 
+						news_date, 
+						CONCAT("' . $chemin_photo . '", 
+						newstable.news_photo) AS news_photo, 
+						CONCAT("' . $chemin_pdf . '", newstable.news_file) AS news_file 
+					FROM 
+						newstable INNER JOIN users ON 
+							(
+								news_entite = users.idEntiteJuridique 
+								OR 
+								news_entite = 0
+							) 
+					WHERE news_publier = 1 
+					AND users.id = ' . $idUser . ' 
+					ORDER BY news_date DESC Limit ' . $tNombre;
 
-        $sql='SELECT news_id,news_titre,news_contenu, news_date, CONCAT("'.$chemin_photo.'", newstable.news_photo) AS news_photo, CONCAT("'.$chemin_pdf.'", newstable.news_file) AS news_file FROM newstable inner join users on (news_entite=users.idEntiteJuridique or news_entite=0) where news_publier=1 and users.id = '.$idUser.' order by news_date DESC Limit '.$nombre;
+			$stmt = $this->getDoctrine()->getManager()->getConnection()->prepare($sql);
+			$stmt->execute();
+			$retour= $stmt->fetchAll();
 
-        $stmt = $this->getDoctrine()->getManager()->getConnection()->prepare($sql);
-        $stmt->execute();
-
-       $retour= $stmt->fetchAll();
-       if(count($retour)==0){
-           $message=array('message'=>'Aucune news trouvée ');
-            return new JsonResponse($message,400);
-       }
-       else 
-       return new JsonResponse($retour);
-
-    }
-
-
+			if (count($retour) == 0) {
+				$message = array('message' => 'Aucune news trouvée ');
+				return new JsonResponse($message, Response::HTTP_BAD_REQUEST);
+			} else {
+				return new JsonResponse($retour, Response::HTTP_OK);
+			}
+		} else {
+			$message = array('message' => 'Paramètre nombre incorrect');
+			return new JsonResponse($message, Response::HTTP_BAD_REQUEST);
+		}
+	}
 }
