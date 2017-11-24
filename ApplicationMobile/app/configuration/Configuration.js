ceb7efe1eccc4c98eddea0b60194af90d98d3901
Adel Noureddine <a.noureddine@cat-amania.com>
2017-11-24 16:52:47 +0100

    corrections et ergonomie

--- ApplicationMobile/app/configuration/Configuration.js
+++ ApplicationMobile/app/configuration/Configuration.js
@@ -5,7 +5,7 @@ var configurationAppli = {
   apiURL: "http://185.57.13.103/rest/web/app_dev.php/",
   lienMdpOublie:
     "https://espacecollaborateur.cat-amania.com/espacecollaborateur/connexion.php",
-  lienSupportJira: "http://jira.svc.cat-amania.com/",
+  lienSupportJira: "http://jira.svc.cat-amania.com/servicedesk/customer/portal/6",
 };
 
 export default configurationAppli;
